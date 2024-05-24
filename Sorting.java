634b70b0091520908ee4361068afb719b6fdeb13
Nick2opxd <nick2op7680@gmail.com>
2024-05-24 10:46:56 +0530

    First Commit

---
+++ Sorting.java
@@ -0,0 +1,34 @@
+package com.nick.sorting;
+
+public class Sorting {
+
+	public static void main(String[] args) {
+		int arr[]= {22,178,6,88,44,99};
+		System.out.print("Before sorting: ");
+		for(int num:arr) {
+			System.out.print(num+ " ");
+		}
+		bubbleSort(arr);
+		System.out.println();
+		System.out.print("After sorting: ");
+		for(int num:arr) {
+			System.out.print(num+ " ");
+		}
+		
+
+	}
+	public static void bubbleSort(int arr[]) {
+		int size=arr.length;
+		int temp=0;
+		for(int i=0;i<size-1;i++) {
+			for(int j=0;j<size-i-1;j++) {
+				if(arr[j]>arr[j+1]) {
+					 temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
+	}
+
+}
